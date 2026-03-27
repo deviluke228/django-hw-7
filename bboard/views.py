972077e31f51cfd2eda10b78ffd14842e5938062
deviluke228 <42192307+deviluke228@users.noreply.github.com>
2026-03-27 08:25:04 +0500

    Первый коммит

---
+++ bboard/views.py
@@ -0,0 +1,42 @@
+from django.db.models import Count
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from bboard.forms import BbForm
+from bboard.models import Bb, Rubric
+
+def index(request):
+    # template = loader.get_template('index.html')
+    bbs = Bb.objects.all()
+    # rubrics = Rubric.objects.all()
+    rubrics = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
+    context = {'bbs': bbs, 'rubrics': rubrics}
+
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    # rubrics = Rubric.objects.all()
+    rubrics = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric':current_rubric}
+
+    return render(request, 'by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'create.html'
+    form_class = BbForm
+    # success_url = '/'
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['rubrics'] = Rubric.objects.all()
+        context['rubrics'] = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
+        return context
