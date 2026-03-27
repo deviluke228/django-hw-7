5319d4be965762d5c9890ceb58ef883ee5b3ea0a
deviluke228 <42192307+deviluke228@users.noreply.github.com>
2026-03-27 08:32:44 +0500

    first commit

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from bboard.views import index, by_rubric, BbCreateView
+from bboard.views import index, by_rubric, BbCreateView, select_columns, exclude_values
 
 urlpatterns = [
     path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('select_columns/', select_columns, name='select_columns'),
+    path('exclude_values/', exclude_values, name='exclude_values'),
     path('', index, name='index'),
 ]
